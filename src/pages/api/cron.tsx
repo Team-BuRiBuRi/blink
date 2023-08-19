ab71912833d08cdaa6b2ae76d570a4efd9ad6392
Me1e <mele0404@naver.com>
2023-08-19 10:48:44 +0900

    feat(cron): cron setting

---
+++ src/pages/api/cron.tsx
@@ -0,0 +1,19 @@
+import { sql } from '@vercel/postgres';
+import { NextApiResponse, NextApiRequest } from 'next';
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  try {
+    const petName = 'petName' + Date.now();
+    const ownerName = 'ownerName' + Date.now();
+
+    await sql`INSERT INTO Pets (Name, Owner) VALUES (${petName}, ${ownerName});`;
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ error });
+  }
+
+  return response.status(204).send('');
+}
