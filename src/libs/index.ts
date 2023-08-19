10e5b1f1e38ffa06f731739615e1109654963080
seojinseojin <1106laura@naver.com>
2023-08-19 18:11:52 +0900

    add(product/[id]): add dashboard ui

---
+++ src/libs/index.ts
@@ -0,0 +1,19 @@
+export const mockGetProduct = (productId: number): GetProductResponse => {
+  return {
+    id: productId,
+    name: 'Tomato',
+    shopId: 2,
+    price: 3000,
+    buyPrice: 2000,
+    buyQuantity: 20,
+    totalSales: 400000,
+    totalSalesQuantity: 15,
+    thumbnail:
+      'https://i.namu.wiki/i/c7fRtsUWjvUJV-E_6tRMcEw1lmxUQecAKRPqCTjuL5dV-nd05DMjqwQskwUa-vl7anSY0QFkonKAM6wwOnBn4rgJEMl3bULh--6YMTIBAvnBGONvGqKLSGp5kt4wxU-7NcNNNFVjZvIatWEAogDtyg.webp',
+  };
+};
+
+export const mockGetExchangeRate = (): AppliedExchangeRate => ({
+  ARS: 1.3,
+  BTC: 1.5,
+});
