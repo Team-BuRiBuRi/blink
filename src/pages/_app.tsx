af30e8c1dbd6e00c8c6e8a7397986654a023d702
seojinseojin <1106laura@naver.com>
2023-08-20 07:07:22 +0900

    add(font): apply pretendard

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import Layout from '@/components/layout';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import 'pretendard/dist/web/variable/pretendardvariable.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider
+      theme={extendTheme({
+        fonts: { heading: 'Pretendard', body: 'Pretendard' },
+      })}
+    >
       <Layout>
         <Component {...pageProps} />
       </Layout>
