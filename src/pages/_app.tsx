365dcb5371e8c02554a83bf6da3255a470e6fbb0
seojinseojin <1106laura@naver.com>
2023-08-20 07:37:33 +0900

    fix(fonts): apply font-size by local fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
+import font from '@/components/fonts';
 import Layout from '@/components/layout';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { Global } from '@emotion/react';
 import type { AppProps } from 'next/app';
-import 'pretendard/dist/web/variable/pretendardvariable.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider
       theme={extendTheme({
-        fonts: { heading: 'Pretendard', body: 'Pretendard' },
+        fonts: {
+          heading: 'Pretendard',
+          body: 'Pretendard',
+        },
       })}
     >
+      <Global styles={font} />
       <Layout>
         <Component {...pageProps} />
       </Layout>
