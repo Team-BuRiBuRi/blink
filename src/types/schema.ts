5fe04b7d73d34ea2f57871aa0d58ecc9d4ab4b20
Me1e <mele0404@naver.com>
2023-08-19 19:25:44 +0900

    feat(type): schema 타입 정정

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,30 +1,35 @@
+/**
+ * numeric은 postgresql에서 사용하는 데이터 타입입니다. 정밀한 숫자를 표현하므로 string 타입으로 받아야 합니다.
+ */
+
 interface Shop {
-    id: number;
-    name: string;
-    thumbnail: string;
-    fee: number; // USD가 아닌 경우
+  id: number; // integer
+  name: string; // character varying
+  thumbnail: string; // text - base64
+  // USD가 아닌 경우 적용
+  fee: string; // numeric
 }
 
 // 모든 price는 USD 기준
 interface Product {
-    id: number;
-    name: string;
-    thumbnail: string;
-    shopId: number;
-    // 판매하고 싶은 금액 - 기계 전부 업데이트
-    price: number;
-    // 살 때 가격 - 고정
-    buyPrice: number;
-    // 살 때 수량 - 고정
-    buyQuantity: number;
-    // 총 매출 - 판매 당시 환율 기준
-    totalSales: number;
-    // 총 판매 수량 - 판매 당시 환율 기준
-    totalSalesQuantity: number;
+  id: number; // integer
+  shopId: number; // integer
+  name: string; // character varying
+  thumbnail: string; // text - base64
+  // 판매하고 싶은 금액 - 기계 전부 업데이트
+  price: string; // numeric
+  // 살 때 가격 - 고정
+  buyPrice: string; // numeric
+  // 살 때 수량 - 고정
+  buyQuantity: number; // integer
+  // 총 매출 - 판매 당시 환율 기준
+  totalSales: string; // numeric
+  // 총 판매 수량 - 판매 당시 환율 기준
+  totalSalesQuantity: number; // integer
 }
 
-// cron으로 보다가 급격한 환율 변동이 있으면 모든 상품 기계 업데이트 하고 최신으로 업데이트
+// cron으로 watching. 급격한 환율 변동이 있으면 모든 상품 기계 업데이트 하고 최신으로 업데이트
 interface AppliedExchangeRate {
-    ARS: number;
-    BTC: number;
+  ARS: string; // numeric
+  BTC: string; // numeric
 }
